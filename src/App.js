4eb210b199c6eaac18c44f2e0db950a7a710f64b
modi0501 <mmayank0501@gmail.com>
2022-02-19 11:51:36 +0530

    Add state and input handlers to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
+import React from "react";
 import "./App.css";
 import Input from "./components/Input/input";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello there</h1>
-      <Input inputSubmitHandler={() => {}} inputChangeHandler={() => {}} />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Hello there</h1>
+//       <Input inputSubmitHandler={() => {}} inputChangeHandler={() => {}} />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTask: "", taskArray: [] };
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    this.inputSubmitHandler = this.inputSubmitHandler.bind(this);
+  }
+
+  inputChangeHandler(event) {
+    this.setState((prevState) => ({
+      ...prevState,
+      currentTask: event.target.value,
+    }));
+  }
+
+  inputSubmitHandler(event) {
+    this.setState((prevState) => ({
+      ...prevState,
+      taskArray: [...prevState.taskArray, prevState.currentTask],
+    }));
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To Do List</h1>
+        <Input
+          inputSubmitHandler={this.inputSubmitHandler}
+          inputChangeHandler={this.inputChangeHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;

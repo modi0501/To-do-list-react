fe3af26dd886ceecddaed4984f8fabaf55c1858c
modi0501 <mmayank0501@gmail.com>
2022-02-21 21:53:17 +0530

    Change TaskList from class to functional component

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,29 +1,22 @@
 import React from "react";
 import Task from "../Task/Task";
 import "./TaskList.css";
-class TaskList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    const taskArray = this.props.taskArray;
-    const taskList = taskArray.map((taskElement, index) => (
-      <Task
-        key={index}
-        taskElement={taskElement}
-        checkboxHandler={(event) =>
-          event && this.props.taskCheckedHandler(index)
-        }
-      />
-    ));
-    return (
-      <div className="taskList">
-        {!taskList.length && <h3>Nothing on your list!</h3>}
-        {taskList.length > 0 && taskList}
-      </div>
-    );
-  }
-}
+const TaskList = (props) => {
+  const taskArray = props.taskArray;
+  const taskList = taskArray.map((taskElement, index) => (
+    <Task
+      key={index}
+      taskElement={taskElement}
+      checkboxHandler={(event) => event && props.taskCheckedHandler(index)}
+    />
+  ));
+  return (
+    <div className="taskList">
+      {!taskList.length && <h3>Nothing on your list!</h3>}
+      {taskList.length > 0 && taskList}
+    </div>
+  );
+};
 
 export default TaskList;

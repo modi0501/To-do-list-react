56bea370efae3ccb37e673bb6a53f32d1aa960ad
modi0501 <mmayank0501@gmail.com>
2022-02-21 21:46:10 +0530

    Change Input and Task from class to functional components

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,29 +1,24 @@
 import React from "react";
 import "./input.css";
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    const inputSubmitHandler = this.props.inputSubmitHandler;
-    const inputChangeHandler = this.props.inputChangeHandler;
-    const value = this.props.value;
-    return (
-      <form onSubmit={inputSubmitHandler} className="form">
-        <input
-          type="text"
-          placeholder="Add a task"
-          onChange={inputChangeHandler}
-          value={value}
-          className="input"
-        />
-        <button type="submit" className="button">
-          Add Task
-        </button>
-      </form>
-    );
-  }
-}
+const Input = (props) => {
+  const inputSubmitHandler = props.inputSubmitHandler;
+  const inputChangeHandler = props.inputChangeHandler;
+  const value = props.value;
+  return (
+    <form onSubmit={inputSubmitHandler} className="form">
+      <input
+        type="text"
+        placeholder="Add a task"
+        onChange={inputChangeHandler}
+        value={value}
+        className="input"
+      />
+      <button type="submit" className="button">
+        Add Task
+      </button>
+    </form>
+  );
+};
 
 export default Input;
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./Task.css";
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const taskElement = this.props.taskElement;
-    return (
-      <div className={!taskElement.removed ? "task" : "task removed"}>
-        <input type="checkbox" onChange={this.props.checkboxHandler} />
-        {taskElement.task}
-      </div>
-    );
-  }
-}
+const Task = (props) => {
+  const taskElement = props.taskElement;
+  const checkboxHandler = props.checkboxHandler;
+  return (
+    <div className={!taskElement.removed ? "task" : "task removed"}>
+      <input type="checkbox" onChange={checkboxHandler} />
+      {taskElement.task}
+    </div>
+  );
+};
 
 export default Task;
